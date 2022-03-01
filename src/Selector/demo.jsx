2a76fcf70cd2fb929a701f8a70102154c94c6240
ze.su <ze.su@tongdun.net>
2022-03-01 20:51:04 +0800

    update: 修改 selector demo

--- src/Selector/demo.jsx
+++ src/Selector/demo.jsx
@@ -1,27 +1,30 @@
 /**
  * compact: true
  */
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Selector } from 'TD-materiel';
 import styles from './demo.less';
 
+const defaultList = Array(30)
+  .fill()
+  .map((_, i) => ({ v: i, checked: false }));
+
 function Demo() {
   const ref = useRef(null);
+  const [list, setList] = useState(defaultList);
 
   function handleSelect(arr) {
-    console.log(arr);
+    setList((list) => list.map((obj, i) => ({ ...obj, checked: arr[i] })));
   }
 
   return (
     <div className={styles.container} ref={ref}>
       <Selector className={styles.selector} scrollRef={ref} onSelect={handleSelect} selection>
-        {Array(20)
-          .fill()
-          .map((_, i) => (
-            <div key={i} className={styles.item}>
-              {i}
-            </div>
-          ))}
+        {list.map(({ v, checked }) => (
+          <div key={v} className={checked ? styles.checked : styles.item}>
+            {v}
+          </div>
+        ))}
       </Selector>
     </div>
   );
