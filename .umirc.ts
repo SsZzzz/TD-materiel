b5e305d05eb70cb7b779a91a12900dae0b6defd8
ze.su <ze.su@tongdun.net>
2022-03-01 20:42:30 +0800

    chore: 修改配置,部署到 github 能正常访问

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/TD-materiel',
+  publicPath: '/TD-materiel/',
   title: 'TD-materiel',
-  favicon: '/logo.png',
-  logo: '/logo.png',
+  favicon: '/TD-materiel/logo.png',
+  logo: '/TD-materiel/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
